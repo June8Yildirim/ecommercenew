f73d61824fd2e432024f206ac0ed90de78e52403
cuneyt.yildirim@dawsoncollege.qc.ca <cuneyt.yildirim@dawsoncollege.qc.ca>
2024-07-28 03:28:01 -0400

    update something

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -27,6 +27,6 @@ router.patch(
 router.patch("updateAvatar", isAuthenticated, updateAvatar);
 router.route("/forget_password").post(forgetPassword).patch(resetPassword);
 router.get("/profile", isAuthenticated, getUserProfile);
-router.get("/logout", isAuthenticated, logOut);
+router.get("/logout", logOut);
 
 export default router;
