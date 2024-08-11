1dac3ea796fac8b93d959376e6001c1e82b109b2
cuneyt.yildirim@dawsoncollege.qc.ca <cuneyt.yildirim@dawsoncollege.qc.ca>
2024-08-10 21:18:48 -0400

    update something0

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -81,7 +81,8 @@ export const createOrder = asyncErrorHandler(async (req, res, next) => {
 
 export const getAdminOrders = asyncErrorHandler(async (req, res, next) => {
   console.log("_____________");
-  const orders = await Order.find({}).populate("owner").populate("productId"); //.populate("Product").populate("User");
+  const orders = await Order.find({}); //.populate("owner").populate("productId"); //.populate("Product").populate("User");
+  const orderss = await Order.find({}).populate("owner").populate("productId"); //.populate("Product").populate("User");
   console.log(JSON.stringify(orders, null, 2));
   res.status(200).json({ orders });
 });
